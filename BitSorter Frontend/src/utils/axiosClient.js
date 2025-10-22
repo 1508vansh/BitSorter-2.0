86b19b30aa14c9b2cc8eac9d4ee4df7176daa99a
Vansh Soni <153891366+1508vansh@users.noreply.github.com>
2025-10-23 00:08:51 +0530

    Update axiosClient.js

--- BitSorter Frontend/src/utils/axiosClient.js
+++ BitSorter Frontend/src/utils/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://bitsorter-2-0.onrender.com',
+  // baseURL: 'http://localhost:5000',
   //baseURL: 'https://bitsorter.onrender.com',
   // baseURL: 'https://bitsorter-production.up.railway.app',
   withCredentials:true,
